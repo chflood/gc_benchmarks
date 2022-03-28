5b71c220dbf4bddd10e37711ad1dfb182749b086
Christine H. Flood <christineflood@juliacomputing.com>
2022-03-28 10:08:11 -0400

    Adding a new benchmark fileio that builds a word frequency count of files from Project Gutenberg.  Updated tree.jl to not regenerate the random numbers.  This includes a symbolic link to /usr/share/dict/words which is almost certainly not portable across implementations.

--- tree.jl
+++ tree.jl
@@ -54,9 +54,7 @@ function sumTree(n::TreeNode)
 end
 
 
-function tree(n)
-    rng = MersenneTwister(12345)
-    temp = rand(rng, Int, n)
+function tree(temp, n)
     root::TreeNode = TreeNode(temp[1])
     for i = 2:n
        insert(temp[i], root)
@@ -64,17 +62,18 @@ function tree(n)
     @printf("\n\nSum of %d numbers = %d\n\n ", n, sumTree(root))
 end
 
-function test(iters, n)
+function test(temp, n, iters)
    for i = 1:iters
-    @time tree(n)
+    @time tree(temp, n)
    end
 end
 
 # iterations is the number of trees
 # tree_size is the number of elements in mb
 rng = MersenneTwister(12345)
+
 iterations = parse(Int64, ARGS[1])
-tree_size = parse(Int64, ARGS[2])
+tree_size = parse(Int64, ARGS[2]) * 1024 * 1024
 
-
-test(iterations, tree_size * 1024 * 1024)
+temp = rand(rng, Int, tree_size)
+test(temp, tree_size, iterations)
